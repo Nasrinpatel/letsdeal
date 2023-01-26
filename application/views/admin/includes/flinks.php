dec52c5a9d55eefa4d99d452d31f07e5e74e44af
Nasrinpatel <nusratp.igenerate@gmail.com>
2023-01-26 23:53:13 +0530

    first commit
    template implement

---
+++ application/views/admin/includes/flinks.php
@@ -0,0 +1,14 @@
+<!-- Vendor js -->
+<script src="<?= base_url('assets/') ?>js/vendor.min.js"></script>
+
+<!-- Plugins js-->
+<script src="<?= base_url('assets/') ?>libs/flatpickr/flatpickr.min.js"></script>
+<script src="<?= base_url('assets/') ?>libs/apexcharts/apexcharts.min.js"></script>
+
+<script src="<?= base_url('assets/') ?>libs/selectize/js/standalone/selectize.min.js"></script>
+
+<!-- Dashboar 1 init js-->
+<script src="<?= base_url('assets/') ?>js/pages/dashboard-1.init.js"></script>
+
+<!-- App js-->
+<script src="<?= base_url('assets/') ?>js/app.min.js"></script>
---
+++ application/views/admin/includes/hlinks.php
@@ -0,0 +1,21 @@
+<meta charset="utf-8" />
+<title>Dashboard | UBold - Responsive Admin Dashboard Template</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
+<meta content="Coderthemes" name="author" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<!-- App favicon -->
+<link rel="shortcut icon" href="<?= base_url('assets/') ?>images/favicon.ico">
+
+<!-- Plugins css -->
+<link href="<?= base_url('assets/') ?>libs/flatpickr/flatpickr.min.css" rel="stylesheet" type="text/css" />
+<link href="<?= base_url('assets/') ?>libs/selectize/css/selectize.bootstrap3.css" rel="stylesheet" type="text/css" />
+
+<!-- Bootstrap css -->
+<link href="<?= base_url('assets/') ?>css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+<!-- App css -->
+<link href="<?= base_url('assets/') ?>css/app.min.css" rel="stylesheet" type="text/css" id="app-style"/>
+<!-- icons -->
+<link href="<?= base_url('assets/') ?>css/icons.min.css" rel="stylesheet" type="text/css" />
+<!-- Head js -->
+<script src="<?= base_url('assets/') ?>js/head.js"></script>
